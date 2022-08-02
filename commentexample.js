ebc0bd3101e61ffbf589fd5c593e677f68899869
SujinJeong <sujin_jeong3@tmax.co.kr>
2022-08-02 10:52:49 +0900

    add: comment style

---
+++ commentexample.js
@@ -0,0 +1,60 @@
+/**
+ * 주석 스타일 예시
+ */
+
+// * JSX components
+/**
+ * 작업기간: // 작업기간은 css에는 표기하지 않는다.  
+ * 작업기간(모바일) // 반응형 작업시 컴포넌트 수정이 들어갈 수 있음
+ * 작업자: 
+ * 
+ * todo: 구현 예정
+ * ! error, trouble, bug to fix or notice
+ * ? logic
+ * * important explanation
+ * 
+ * @param
+ * @async
+ * @returns
+ * 
+ * @readonly
+ * @exports
+ * @imports
+ * 
+ * @summary
+ * 
+ * import 는 scss 파일을 가장 마지막으로 Component 시작 전에 두 번 enter
+ */
+
+
+
+
+// * scss stylesheets
+
+/**
+ * * class name rule
+ * focused - focus 됐을 때,
+ */
+
+/**
+ * *반응형 작업
+ * 해당 page 아래에 코드 붙여넣기 
+ */
+
+/* 
+// * 👽️responsive
+@include mobile-max{
+}
+*/
+
+
+// * file name
+/**
+ * 숫자는 01 02 03 순으로 증가 
+ * icon(.svg) - /public/asset/icon - icon_naming
+ *  .icon 
+ * svg(.svg) - /public/asset/svg - svg_naming 
+ *  .svg 
+ *  ? 상위 DOM의 width와 height를 상속받도록 함
+ *  기존 svg 태그의 Attribute style은 삭제하도록 함
+ */
