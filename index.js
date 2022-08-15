fa4016877caf4a94e0dd7c309ef3a0b0f0d52224
sinri <1lliodqb@gmail.com>
2022-08-15 16:33:03 +0900

    Initializing

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+  // const root = document.querySelector('#root');
+
+  const div = document.createElement('div');
+  div.innerHTML = 'javascript lecture';
+
+  document.body.appendChild(div);
+}
