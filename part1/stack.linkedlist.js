788b5d56f5ec1b7994f3eb6fbab68db00a426957
sinri <1lliodqb@gmail.com>
2022-08-28 17:48:26 +0900

    stack linked list example

---
+++ part1/stack.linkedlist.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    const node = new Node(value);
+    node.next = this.top;
+
+    this.top = node;
+    this.size += 1;
+  }
+
+  pop() {
+    const value = this.top.value;
+    this.top = this.top.next;
+    this.size -= 1;
+    return value;
+  }
+
+  size() {
+    return this.size;
+  }
+}
