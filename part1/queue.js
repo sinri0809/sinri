2836939f89401d50b8115a2bf6425e9de7e50432
sinri <1lliodqb@gmail.com>
2022-08-28 22:17:43 +0900

    queue example

---
+++ part1/queue.js
@@ -0,0 +1,28 @@
+// 배열 크기가 무한정 늘어날 수 있는 단점 있음.
+
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
+
+  dequeue() {
+    const value = this.queue[this.front];
+    delete this.queue[this.front];
+    this.front += 1;
+    return value;
+  }
+
+  peek() {
+    return this.queue[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+}
