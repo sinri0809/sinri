dadfd0fc89905e74ab3d325deffe21d941089112
sinri <1lliodqb@gmail.com>
2022-12-10 16:30:15 +0900

    Organizing style directories

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 import 'style/index.scss';
 
 const root = ReactDOM.createRoot(
