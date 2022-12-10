e5b46d689265bcd4af9da7769b20089f3e2cc673
sinri <1lliodqb@gmail.com>
2022-12-10 21:06:08 +0900

    Font System

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,7 +1,13 @@
 const Home = () => {
-  return <div>
+  return <main>
     this  is home
-  </div>
+    <p className="font-thin">font thin <em>italic</em></p>
+    <p className="font-light">font light <em>italic</em></p>
+    <p className="font-regular">font regular <em>italic</em></p>
+    <p className="font-medium">font medium <em>italic</em></p>
+    <p className="font-bold">font bold <em>italic</em></p>
+    <p className="font-black">font black <em>italic</em></p>
+  </main>
 }
 
 export default Home;
