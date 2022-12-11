c903e3550050a6eb63478c931c88f4be59f5ab5e
sinri <1lliodqb@gmail.com>
2022-12-11 22:33:52 +0900

    Input System

--- src/pages/PageBoard.tsx
+++ src/pages/PageBoard.tsx
@@ -1,7 +1,28 @@
+import React, { useRef, useState, useTransition } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const Board = () => {
+  const [isPending, startTransition] = useTransition();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputValue, setInputValue] = useState("");
+
+  const onChagenInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    startTransition(() => setInputValue(e.target.value))
+  };
+
   return <section className="board-container">
+    <input
+      ref={inputRef}
+      value={inputValue}
+      onChange={onChagenInput}
+      type="text"
+      placeholder="search"
+      required={true}
+      // readOnly={false}
+      className="input"
+    />
+    {isPending && "Loading"}
     <Link to="/board/1">1 board</Link>
     <Outlet />
   </section>
